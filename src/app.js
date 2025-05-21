306dfdd60c36e0825a54f843e40553c70ac0c01d
Francisco Erlyson Pamplona <77758434+DreamEater360@users.noreply.github.com>
2025-05-20 22:02:56 -0300

    terminado

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ function cadastrarAluno(){
     const senha = document.querySelector('#senha').value
     const email = document.querySelector('#email').value
     const cpf = document.querySelector('#cpf').value
+    const avisoCadastro = document.querySelector('#cadastro')
 
     let obj = {
         id: gerarId(),
@@ -27,6 +28,9 @@ function cadastrarAluno(){
     localStorage.setItem(obj.id, objJson)
     localStorage.setItem('emailIndex:' + email, obj.id);
 
+    avisoCadastro.innerHTML = "Cadastro realizado com sucesso!!!"
+    avisoCadastro.style.color = "green"
+
 }
 
 // função para efetuar login do aluno
@@ -34,14 +38,21 @@ function loginAluno(){
 
     const email = document.querySelector('#email').value
     const senha = document.querySelector('#senha').value
+    const avisoLogin = document.querySelector('#login')
 
     const aluno = checarAluno(email, senha)
   
     if (aluno){
-       console.log("login efetuado com sucesso!!!")
-       window.location.href = "src\HTML\biblioteca.htm"
+        sessionStorage.setItem('alunoLogado', aluno.id);
+
+        console.log("login efetuado com sucesso!!!")
+        avisoLogin.innerHTML = "login efetuado com sucesso!!!"
+        avisoLogin.style.color = "green"
+        window.location.href = "biblioteca.html"
     } else {
         console.log("Email ou senha incorretos!!!")
+        avisoLogin.innerHTML = "Email ou senha incorretos!!!"
+        avisoLogin.style.color = "red"
     }
    
 }
@@ -61,41 +72,94 @@ function checarAluno(email, senha){
     }
 }
 
+const inventario = [
+  "Dom Quixote",
+  "O Senhor dos Anéis",
+  "O Pequeno Príncipe",
+  "1984",
+  "Cem Anos de Solidão",
+  "Orgulho e Preconceito",
+  "O Hobbit",
+  "A Odisséia",
+  "Crime e Castigo",
+  "Moby Dick",
+  "A Revolução dos Bichos",
+  "O Apanhador no Campo de Centeio",
+  "O Grande Gatsby",
+  "Admirável Mundo Novo",
+  "A Arte da Guerra",
+  "O Alquimista",
+  "Eu, Robô",
+  "Duna",
+  "Neuromancer",
+  "O Nome do Vento"
+];
+
 // função para verificar quais livros estao disponiveis na biblioteca
-function listarLivros(inv){
-
-    for (i = 0; i < inv.lenght; i++){
-        console.log(inv[i])
-    }
-
+function listarLivros() {
+  const listaLivros = document.getElementById("inventario") 
+  listaLivros.innerHTML = "" 
+  
+  inventario.forEach(livro => {
+      const itemLista = document.createElement("li");
+      itemLista.textContent = livro;
+      listaLivros.appendChild(itemLista);
+  });
 }
 
-// função para pegar livro emprestado
-function emprestimoLivro(inv, aluno){
+// Função para pegar livro emprestado
+function emprestimoLivro() {
+    const aluno = getAlunoLogado()
+    const avisoEmprestimo = document.querySelector('#emprestimo')
 
-    if (aluno.livro !== ""){
-        console.log("Voce ja esta com um livro emprestado, devolva antes de pegar outro!!!")
+    if (aluno.livro !== "") {
+        avisoEmprestimo.innerHTML = "Você já está com um livro emprestado!!!"
+        return
+    }
+
+    const livroDesejado = document.getElementById('inpLivro').value;
+
+    if (inventario.includes(livroDesejado)) {
+        aluno.livro = livroDesejado;
+
+        inventario = inventario.filter(livro => livro !== livroDesejado);
+
+        localStorage.setItem(aluno.id, JSON.stringify(aluno));
+        listarLivros()
+        avisoEmprestimo.innerHTML = "Livro emprestado com sucesso!!!"
     } else {
-        console.log("Informe qual dos livros diponiveis voce deseja: ")
-        listarLivros(inv)
-        aluno.livro = Input.value // definir de onde pegar o input
-
-        localStorage.setItem(aluno.id, aluno)
+        avisoEmprestimo.innerHTML = "Livro não encontrado no inventário!!!"
     }
-
 }
 
+// Função para devolver livro 
+function devolverLivro() {
+    const aluno = getAlunoLogado()
+    const avisoDevolucao = document.querySelector('#devolver')
 
-// função para devolver livro
-function devolverLivro(aluno){
-
-    if (aluno.livro === ""){
-        console.log("Voce nao esta com um livro emprestado!!!")
+    if (aluno.livro === "") {
+        avisoDevolucao.innerHTML = "Você não tem livros para devolver!!!"
+        return
     } else {
-        aluno.livro = "" 
-        console.log("Voce devolveu o livro com sucesso!!!")
+        inventario.push(aluno.livro)
 
-        localStorage.setItem(aluno.id, aluno)
+        aluno.livro = ""
+        localStorage.setItem(aluno.id, JSON.stringify(aluno))
+        listarLivros()
+        avisoDevolucao.innerHTML = "Livro devolvido com sucesso!!!"
     }
+
 }
 
+// Função para obter o aluno logado
+function getAlunoLogado() {
+    const alunoId = sessionStorage.getItem('alunoLogado')
+    if (alunoId) {
+        const alunoJson = localStorage.getItem(alunoId)
+        if(alunoJson) {
+            return JSON.parse(alunoJson)
+        }
+    } else {
+      return null
+    }
+}
